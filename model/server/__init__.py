81671196eff4da337fdea8579123665ddf09282a
Ram Goli <ramsgoli@gmail.com>
2020-05-14 17:35:03 -0700

    Got the flask shit working

---
+++ model/server/__init__.py
@@ -0,0 +1,24 @@
+from flask import Flask, request
+import joblib
+import os
+
+def create_app():
+    app = Flask(__name__)
+
+    print("HERE")
+    vectorizer = joblib.load(os.path.join(app.root_path, 'Vectorizer.joblib.bz2'))
+    clf = joblib.load(os.path.join(app.root_path, 'LogisticRegression.joblib.bz2'))
+    print("DONE")
+
+    @app.route('/sentiment', methods=['POST'])
+    def sentiment():
+        req = request.json
+        text = req['text']
+
+        vectorized = vectorizer.transform([text])
+        pred = clf.predict(vectorized)
+        return {
+            'response': 'positive' if pred[0] == 1 else 'negative'
+        }
+
+    return app
